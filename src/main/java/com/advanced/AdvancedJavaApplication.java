13bb4a04567c60f990c89f500dea9969170794b7
minister.ik <minister.ik@aptner.com>
2022-06-24 19:59:21 +0900

    first commit

---
+++ src/main/java/com/advanced/AdvancedJavaApplication.java
@@ -0,0 +1,13 @@
+package com.advanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdvancedJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/advanced/AdvancedJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.advanced;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
