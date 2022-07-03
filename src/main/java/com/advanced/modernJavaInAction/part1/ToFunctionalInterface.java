8520a4d77eaa256851d10c95811a6fabcc91ed40
minister.ik <minister.ik@aptner.com>
2022-07-03 21:33:40 +0900

    feat: 오토박싱을 하지 않는 기본형 함수형 인터페이스(#5)

--- src/main/java/com/advanced/modernJavaInAction/part1/ToFunctionalInterface.java
+++ src/main/java/com/advanced/modernJavaInAction/part1/ToFunctionalInterface.java
@@ -1,9 +1,20 @@
 package com.advanced.modernJavaInAction.part1;
 
+import org.springframework.stereotype.Service;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.IntPredicate;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
+@Service
 public class ToFunctionalInterface {
 
     /**
@@ -40,4 +51,52 @@ public class ToFunctionalInterface {
         }
     }
 
+    /**
+     * 대표적인 자바 8 함수형 인터페이스
+     *  - Predicate : Boolean 표현식이 필요한 상황에서 사용
+     *  - Consumer  : T 형식의 객체를 인수로 받아서 어떤 동작을 수행하고 싶을 때 사용(return void)
+     *  - Function  : T 형식의 객체를 인수로 받아서 R 형식의 객체를 반환하는 apply 메서드 제공
+     */
+    public <T> List<T> filter(List<T> list, Predicate<T> p) {
+        return list.stream()
+                .filter(p)
+                .collect(Collectors.toList());
+    }
+
+    public <T> void forEach(List<T> list, Consumer<T> c) {
+        for (T t : list) {
+            c.accept(t);
+        }
+    }
+    public void testConsumer() {
+        forEach(Arrays.asList(1, 2, 3, 4, 5), System.out::println);
+    }
+
+    public <T, R> List<R> map(List<T> list, Function<T, R> f) {
+        List<R> result = new ArrayList<>();
+        for (T t : list) {
+            result.add(f.apply(t));
+        }
+        return result;
+    }
+    public void testFunction() {
+        List<Integer> result = map(Arrays.asList("1", "2", "3"), Integer::parseInt);
+    }
+
+    /**
+     * 기본형을 입출력으로 사용하는 경우 오토박싱을 피할 수 있는 특별한 함수형 인터페이스를 제공함
+     */
+    public void primitiveTypeInterface() {
+        // 박싱 없음
+        IntPredicate evenNumbers = (int i) -> i % 2 == 0;
+        printResult(evenNumbers.test(1000));
+
+        // 박싱
+        Predicate<Integer> oddNumbers = (Integer i) -> i % 2 != 0;
+        printResult(oddNumbers.test(1000));
+    }
+
+    public void printResult(boolean result) {
+        System.out.println(result? "TRUE" : "FALSE");
+    }
 }
